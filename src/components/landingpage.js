db763acc86b9058cdfd54b9f5f9809275c9d5747
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-08-05 17:07:50 -0400

    updated landing page and app.css

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
   render() {
     return(
-      <div><h1>Landing Page</h1></div>
+      <div>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img 
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQkdokSVm6ytjniDQmDx1sVmRiMsgoWyqcIdw&usqp=CAU"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr/>
+
+              <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB</p>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
